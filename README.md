7d5f8989f0868122656c27d2f9bd751d5687c522
Sofia Javed <sofjaved@gmail.com>
2021-01-02 19:16:11 -0500

    completed submission, includes script, README with link to reflections document, and sample output.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Scale-Field-Engineering-Interview-Technical-Check
-Sofia Javed's submission for Scale's field engineering interview technical check
+
+[Reflection on future quality checks.](https://docs.google.com/document/d/1FiqnkiXVjJsvFi6cdFsnlTSUslOMWtw-Pkaax5MyKIA/edit?usp=sharing)
+
+Sofia Javed - Scale Field Engineering Interview Technical Check
+Client: **ObserveSign**
+Scale project name: **Traffic Sign Detection**
+
+**Overview:**
+
+**ObserveSign** asked **Scale** to annotate a number of images according to specific guidelines. Scale completed the tasks, but there may be some errors in the annotations.
+
+This script fetches all the completed tasks associated with the project name **“Traffic Sign Detection”** and runs a series of functions to see if the annotations have been completed correctly according to the client’s guidelines. Specifically, this script runs five quality checks:
+
+1)	**Check Box Size** checks the sizes of the bounding boxes in each completed task. If a box is too large and/or takes up most of the image, it may be an error.
+2)	**Check Label** checks the labels on the bounding boxes/annotations in each completed task. If a label does not match any of the options listed in the client’s guidelines, it may be an error.
+3)	**Check Occlusion** checks the occlusion attribute of the annotations in each completed task. If there is no occlusion percentage annotated, if the occlusion percentage does not match any of the options listed in the client’s guidelines, it may be an error. If the percentage is too high, it may be an error because most of the sign will not be visible.
+4)	**Check Truncation** checks the truncation attribute of the annotations in each completed task. If there is no truncation percentage annotated, if the truncation percentage does not match any of the options listed in the client’s guidelines, it may be an error. If the percentage is too high, it may be an error because most of the sign will not be included in the image.
+5)	**Check Background Color** checks the background color attribute of the annotations in each completed task. If the background color does not match any of the options listed in the client’s guidelines, it may be an error. Per the client’s guidelines, an annotation with a label of “non_visible_face” must list “not_applicable” as the background color; otherwise, it may be an error.
+
+The script outputs a report in a JSON file (**Traffic_Sign_Detection_Quality_Checks.json**). The report lists the task IDs for which there are certain or potential annotation errors. For each listed task ID, the report lists the quality checks for which there may be errors and for each quality check, the UUIDs of the bounding boxes/annotations that either have *certain errors* – listed as **“errors_uuids”** – or those that have *potential errors* – listed as **“warnings_uuids.”**
+
+To generate a quality checks report for the “Traffic Sign Detection” project, uncomment the following line at the top of the script before running the script:
+
+//qualityChecks(“Traffic Sign Detection);
+
+**NOTE:** The fetch tasks API call displays a maximum of 100 results per page. If the project has more than 100 images/task responses, the code must be adjusted to run the checks for 100 tasks at a time.
---
+++ qualityChecks.js
@@ -0,0 +1,282 @@
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+//NOTE: To generate a quality checks report for the Traffic Sign Detection project, uncomment the line below and run the script:
+qualityChecks("Traffic Sign Detection");
+
+//Fetch all tasks in the project and run quality checks:
+
+function qualityChecks(projectName) {
+  let url = "https://api.scale.com/v1/tasks?project=" + projectName.split(" ").join("%20");
+  fetch(url, {
+    "method": "GET",
+    "headers": {
+      "Authorization": "Basic bGl2ZV83NDI3NWI5YjJiOGI0NGQ4YWQxNTZkYjAzZDIwMDhlZDo="
+    }
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(response => {
+   return runChecks(projectName, response.docs);
+  })
+  .then(response => {
+    let fileName = projectName.split(" ").join('_') + '_Quality_Checks.json'
+    fs.writeFile(fileName, JSON.stringify(response, null, 4), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+        console.log(`File has been created: ${fileName}`);
+      });
+   })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+//Helper functions:
+
+//Run all quality checks for all tasks:
+
+function runChecks(projectName, tasks) {
+  let report = {
+    projectName: projectName,
+    qualityChecks: []
+  };
+  for(let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    if(singleTaskChecks(task)) {
+      report.qualityChecks.push(singleTaskChecks(task));
+    };
+  }
+  return report;
+}
+
+//Run all quality checks for a single task:
+
+function singleTaskChecks(task) {
+  let singleTaskReport = {};
+  let boxes = task.response.annotations;
+
+  //loop through all annotations and run all checks on each:
+  for(let i = 0; i < boxes.length; i++) {
+    let box = boxes[i];
+
+    //check box size:
+    if(checkBoxSize(box)) {
+      if(singleTaskReport.task_id === undefined) {
+        singleTaskReport.task_id = task.task_id;
+      }
+      if(singleTaskReport.checkBoxSize !== undefined) {
+        if(checkBoxSize(box)[0] === 'warning') {
+         if(singleTaskReport.checkBoxSize.warnings_uuids) {
+           singleTaskReport.checkBoxSize.warnings_uuids.push(checkBoxSize(box)[1]);
+         } else {
+          singleTaskReport.checkBoxSize.warnings_uuids = [checkBoxSize(box)[1]];
+         }
+        }
+        if(checkBoxSize(box)[0] === 'error') {
+          if(singleTaskReport.checkBoxSize.errors_uuids) {
+            singleTaskReport.checkBoxSize.errors_uuids.push(checkBoxSize(box)[1]);
+          } else {
+           singleTaskReport.checkBoxSize.errors_uuids = [checkBoxSize(box)[1]];
+          }
+        }
+      } else {
+        if(checkBoxSize(box)[0] === 'warning') {
+          singleTaskReport.checkBoxSize = {
+            warnings_uuids: [checkBoxSize(box)[1]]
+          }
+        }
+        if(checkBoxSize(box)[0] === 'error') {
+          singleTaskReport.checkBoxSize = {
+            errors_uuids: [checkBoxSize(box)[1]]
+          }
+        }
+      }
+    }
+
+    //check label:
+    if(checkLabel(box)) {
+      if(singleTaskReport.task_id === undefined) {
+        singleTaskReport.task_id = task.task_id;
+      }
+      if(singleTaskReport.checkLabel !== undefined) {
+        singleTaskReport.checkLabel.errors_uuids.push(checkLabel(box)[1]);
+      } else {
+        singleTaskReport.checkLabel = {
+          errors_uuids: [checkLabel(box)[1]]
+        }
+      }
+    }
+
+    //check occlusion:
+    if(checkOcclusion(box)) {
+      if(singleTaskReport.task_id === undefined) {
+        singleTaskReport.task_id = task.task_id;
+      }
+      if(singleTaskReport.checkOcclusion !== undefined) {
+        if(checkOcclusion(box)[0] === 'warning') {
+          if(singleTaskReport.checkOcclusion.warnings_uuids) {
+            singleTaskReport.checkOcclusion.warnings_uuids.push(checkOcclusion(box)[1]);
+          } else {
+            singleTaskReport.checkOcclusion.warnings_uuids = [checkOcclusion(box)[1]];
+          }
+        }
+        if(checkOcclusion(box)[0] === 'error') {
+          if(singleTaskReport.checkOcclusion.errors_uuids) {
+            singleTaskReport.checkOcclusion.errors_uuids.push(checkOcclusion(box)[1]);
+          } else {
+            singleTaskReport.checkOcclusion.errors_uuids = [checkOcclusion(box)[1]];
+          }
+        }
+      } else {
+        if(checkOcclusion(box)[0] === 'warning') {
+          singleTaskReport.checkOcclusion = {
+            warnings_uuids: [checkOcclusion(box)[1]]
+          }
+        }
+        if(checkOcclusion(box)[0] === 'error') {
+          singleTaskReport.checkOcclusion = {
+            errors_uuids: [checkOcclusion(box)[1]]
+          }
+        }
+      }
+    }
+
+    //check truncation:
+    if(checkTruncation(box)) {
+      if(singleTaskReport.task_id === undefined) {
+        singleTaskReport.task_id = task.task_id;
+      }
+      if(singleTaskReport.checkTruncation !== undefined) {
+        if(checkTruncation(box)[0] === 'warning') {
+          if(singleTaskReport.checkTruncation.warnings_uuids) {
+            singleTaskReport.checkTruncation.warnings_uuids.push(checkTruncation(box)[1]);
+          } else {
+            singleTaskReport.checkTruncation.warnings_uuids = [checkTruncation(box)[1]];
+          }
+        }
+        if(checkTruncation(box)[0] === 'error') {
+          if(singleTaskReport.checkTruncation.errors_uuids) {
+            singleTaskReport.checkTruncation.errors_uuids.push(checkTruncation(box)[1]);
+          } else {
+            singleTaskReport.checkTruncation.errors_uuids = [checkTruncation(box)[1]];
+          }
+        }
+      } else {
+        if(checkTruncation(box)[0] === 'warning') {
+          singleTaskReport.checkTruncation = {
+            warnings_uuids: [checkTruncation(box)[1]]
+          }
+        }
+        if(checkTruncation(box)[0] === 'error') {
+          singleTaskReport.checkTruncation = {
+            errors_uuids: [checkTruncation(box)[1]]
+          }
+        }
+      }
+    }
+
+    //check background color:
+    if(checkBackgroundColor(box)) {
+      if(singleTaskReport.task_id === undefined) {
+        singleTaskReport.task_id = task.task_id;
+      }
+      if(singleTaskReport.checkBackgroundColor !== undefined) {
+        singleTaskReport.checkBackgroundColor.errors_uuids.push(checkBackgroundColor(box)[1]);
+      } else {
+        singleTaskReport.checkBackgroundColor = {
+          errors_uuids: [checkBackgroundColor(box)[1]]
+        }
+      }
+    }
+
+ }
+
+  //return a report for one task if there are any warnings or errors:
+  if(Object.keys(singleTaskReport).length > 0) {
+  return singleTaskReport;
+  }
+}
+
+
+//Quality Checks (5):
+
+// (1) If the bounding box takes up most of the image, it may be an error.
+
+function checkBoxSize(box) {
+    if(box.width >= 1400 || box.height >= 700) {
+      return ['error', box.uuid];
+    }
+    if(box.width >= 750 && box.width < 1400) {
+      return ['warning', box.uuid];
+    }
+    if(box.height >= 400 && box.height <= 700) {
+      return ['warning', box.uuid];
+    }
+  }
+
+// (2) If label is not included or is not any of the client's given options, it may be an error.
+
+function checkLabel(box) {
+  let options = ['traffic_control_sign', 'construction_sign', 'information_sign', 'policy_sign', 'non_visible_face'];
+  let label = box.label;
+  if(label === undefined) {
+    return ['error', box.uuid]
+  } else if(!options.includes(label)) {
+    return ['error', box.uuid]
+  }
+}
+
+// (3) If occlusion is not included or percentage is too high, most of the sign is not visible.
+
+function checkOcclusion(box) {
+  let options = ['0%', '25%','50%', '75%', '100%'];
+  let occlusion = box.attributes.occlusion;
+  if(occlusion === undefined || !options.includes(occlusion)) {
+    return ['error', box.uuid];
+  } else {
+    let occlusionNum = Number(occlusion.slice(0, occlusion.length - 1));
+      if(occlusionNum >= 75) {
+      return ['error', box.uuid];
+      }
+      if(occlusionNum === 50) {
+      return ['warning', box.uuid];
+      }
+    }
+}
+
+// (4) If truncation is not included or percentage is too high, most of the sign is not included in the image.
+
+function checkTruncation(box) {
+  let options = ['0%', '25%','50%', '75%', '100%'];
+  let truncation = box.attributes.truncation;
+  if(truncation === undefined || !options.includes(truncation)) {
+    return ['error', box.uuid];
+  } else {
+    let truncationNum = Number(truncation.slice(0, truncation.length - 1));
+    if(truncationNum >= 75) {
+      return ['error', box.uuid];
+    }
+    if(truncationNum === 50) {
+      return ['warning', box.uuid];
+    }
+  }
+}
+
+// (5) If background color is not included or is not any of the client's given options, it may be an error.
+
+function checkBackgroundColor(box) {
+  let options = ['white', 'red', 'orange', 'green', 'blue', 'other', 'not_applicable'];
+  let background_color = box.attributes.background_color;
+  if(background_color === undefined) {
+    return ['error', box.uuid];
+  } else if(!options.includes(background_color)) {
+      return ['error', box.uuid];
+  } else if(box.label === 'non_visible_face' && background_color !== 'not_applicable') {
+    return ['error', box.uuid];
+  }
+}
+
